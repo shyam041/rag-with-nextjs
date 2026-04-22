4c9f2cd640570c885e9052956eede9e1fac909d2
Shyam Deshpande <shyamd@nousinfo.com>
2026-04-22 12:36:54 +0530

    fix: use additive reducer for retrievalAttempts counter field

--- rag/state.ts
+++ rag/state.ts
@@ -8,7 +8,7 @@ export const RAGStateAnnotation = Annotation.Root({
   refinedQueries: Annotation<string[]>({ reducer: (_, b) => b, default: () => [] }),
   documents: Annotation<SourceDetail[]>({ reducer: (_, b) => b, default: () => [] }),
   llmResponse: Annotation<string>({ reducer: (_, b) => b, default: () => "" }),
-  retrievalAttempts: Annotation<number>({ reducer: (_, b) => b, default: () => 0 }),
+  retrievalAttempts: Annotation<number>({ reducer: (a, b) => a + b, default: () => 0 }),
   citedDocumentIds: Annotation<string[]>({ reducer: (_, b) => b, default: () => [] }),
   filteredSources: Annotation<SourceDetail[]>({ reducer: (_, b) => b, default: () => [] }),
   responseConfidence: Annotation<number>({ reducer: (_, b) => b, default: () => 0 }),
